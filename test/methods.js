e28842bc6bd31185761d598b4ab277a8f1109dd8
Jack Tuck <jack-tuck@hotmail.com>
2016-06-13 21:45:18 +0100

    Improved tests

--- test/methods.js
+++ test/methods.js
@@ -3,44 +3,42 @@ var util = require('util');
 var chai = require('chai');
 var expect = chai.expect;
 var Fleeting = require('../index');
-var fleeting = new Fleeting({});
+var fleeting;
 
 beforeEach(function() {
-  fleeting.purge();
+  fleeting = new Fleeting({});
 });
 
 describe('fleeting', function() {
   describe('set', function() {
     it('should insert node at head', function() {
-      fleeting.set(1, 1);
-      expect(fleeting._linkedlist._head.data.k).to.equal(1);
+      expect(fleeting.set('k1', 'v1')).to.equal('v1');
     });
   });
 
   describe('purge', function() {
     it('should purge caches', function() {
       expect(fleeting._cache).to.deep.equal({});
-      expect(fleeting._linkedlist._head).to.equal(null);
-      expect(fleeting._linkedlist._tail).to.equal(null);
+      expect(fleeting._linkedlist._length).to.equal(0);
     });
   });
 
   describe('del', function() {
     it('should delete node', function() {
-      fleeting.set(1, 1);
-      fleeting.del(1);
+      fleeting.set('k1', 'v1');
+      expect(fleeting.del('k1')).to.equal('v1');
+    });
 
-      expect(fleeting._linkedlist._head).to.equal(null);
-      expect(fleeting._linkedlist._tail).to.equal(null);
+    it('should try to delete non-existant node', function() {
+      expect(fleeting.del('k1')).to.equal(undefined);
     });
   });
 
   describe('get', function() {
     it('should get value of node', function() {
-      fleeting.set(5, 5);
+      fleeting.set('k1', 'v1');
 
-      expect(fleeting.get(5)).to.equal(5);
-      expect(fleeting.get(1)).to.equal(undefined);
+      expect(fleeting.get('k1')).to.equal('v1');
     });
 
     it('should bolster node to head', function(done) {
@@ -54,6 +52,10 @@ describe('fleeting', function() {
         done();
       }, 50);
     });
+
+    it('should try to get non-existant node', function() {
+      expect(fleeting.get('k')).to.equal(undefined);
+    });
   });
 
   describe('peek', function() {
@@ -64,13 +66,14 @@ describe('fleeting', function() {
     });
 
     it('should not bolster node to head', function(done) {
-      fleeting.set(1, 1);
-      fleeting.set(2, 2);
+      fleeting.set('k1', 'v1');
+      fleeting.set('k2', 'v2');
 
-      fleeting.peek();
+      fleeting.peek('k1');
 
       setTimeout(function() {
-        expect(fleeting._linkedlist._head.data.k).to.not.equal(1);
+        expect(fleeting._linkedlist._head.data.k).to.equal('k2');
+        expect(fleeting._linkedlist._head.data.k).to.not.equal('k1');
         done();
       }, 50);
     });
@@ -78,19 +81,19 @@ describe('fleeting', function() {
 
   describe('has', function() {
     it('should check if node is existent', function() {
-      fleeting.set(1, 1);
+      fleeting.set('k1', 'v1');
 
-      expect(fleeting.has(1)).to.equal(true);
-      expect(fleeting.has(2)).to.equal(false);
+      expect(fleeting.has('k1')).to.equal(true);
+      expect(fleeting.has('k2')).to.equal(false);
     });
 
     it('should not bolster node to head', function() {
-      fleeting.set(1, 1);
-      fleeting.set(2, 2);
+      expect(fleeting.set('k1', 'v1')).to.equal('v1');
+      expect(fleeting.set('k2', 'v2')).to.equal('v2');
 
-      fleeting.has(1);
+      expect(fleeting.has('k1')).to.equal(true);
 
-      expect(fleeting._linkedlist._head.data.k).to.not.equal(1);
+      expect(fleeting._linkedlist._head.data.k).to.not.equal('k1');
     });
   });
 });
