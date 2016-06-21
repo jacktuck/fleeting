5b14938de2013264778b3a5e1af172683c8f7970
Jack Tuck <jack-tuck@hotmail.com>
2016-06-21 22:56:34 +0100

    Cleaned up usage.js example

--- example/usage.js
+++ example/usage.js
@@ -1,25 +1,35 @@
 var Fleeting = require('../index.js');
+
+/*
+    Here we set a ttl of 3 seconds and the max number of nodes to 5
+*/
 var fleeting = new Fleeting({
   ttl: 3000,
   max: 5
 });
 
-fleeting.on('evicted', function(value) {
-  console.log('evicted -> value', value);
+fleeting.on('evicted', function(i) {
+  console.log('Evicted node: ', i);
 });
 
+/*
+    Add 10 nodes to the cache with 0-9 as their keys and values. Notice that
+    once we have 0-4 (5 nodes), we start to evict them least-recently-used nodes
+    to make way for nodes 5-9.
+*/
 for (var i = 0; i < 10; i++) {
-  console.log('set -> ', i, ': ', fleeting.set(i, {i: i}));
+  console.log('Added node: ', i);
+  fleeting.set(i, i);
 }
 
-for (var i = 0; i < 10; i++) {
-  console.log('get -> ', i, ': ', fleeting.get(i));
-}
-
- console.log('del -> ', 9, ': ', fleeting.del(9));
-
+/*
+    At this point, we are left with nodes 5-9, which should also be evicted
+    once they expire. Here we're expiring after 3 seconds, so I do a setTimeout
+    to execute a .get on nodes 5-9 in 3 seconds time.
+*/
 setTimeout(function() {
   for (var i = 5; i < 10; i++) {
-    console.log('get (now expired) -> ', i, ': ', fleeting.get(i));
+    console.log('Getting node: ', i);
+    fleeting.get(i);
   }
 }, 3000);
